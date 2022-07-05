2c09e1ec02000c6369fce6ea374d52ec0a391e78
Tuan Nguyen <tuanpham.25201@gmail.com>
2022-07-05 21:23:13 +0700

    fix hình trong room-detail, modal trong trang-cá-nhân

--- Motel-Management/web/assets/js/user-script.js
+++ Motel-Management/web/assets/js/user-script.js
@@ -328,4 +328,49 @@ $(function () {
   });
 });
 
+//truncate content
 
+$(document).ready(function () {
+  (function () {
+    var showChar = 0;
+    var ellipsestext = "";
+
+    $(".truncate-content").each(function () {
+      var content = $(this).html();
+      if (content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content;
+        var html =
+          '<div class="truncate-text" style="display:block">' +
+          c +
+          '<span class="moreellipses">' +
+          ellipsestext +
+          '&nbsp;&nbsp;<a href="" class="moreless more">Xem thêm</a></span></span></div><div class="truncate-text" style="display:none">' +
+          h +
+          '<a href="" class="moreless less">Thu gọn</a></span></div>';
+
+        $(this).html(html);
+      }
+    });
+    var getParentTruncateContent = document
+      .querySelector(".truncate-content")
+      .parentElement.querySelector(".desc");
+    $(".moreless").click(function () {
+      var thisEl = $(this);
+      var cT = thisEl.closest(".truncate-text");
+      var tX = ".truncate-text";
+
+      if (thisEl.hasClass("less")) {
+        cT.prev(tX).toggle();
+        cT.slideToggle();
+      } else {
+        cT.toggle();
+        cT.next(tX).fadeToggle();
+      }
+      return false;
+    });
+    /* end iffe */
+  })();
+
+  /* end ready */
+});
