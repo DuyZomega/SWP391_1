d3cb89315d66cabce247c03e1ba8475efbc421fb
PhuongThuyy <74039932+CTPhuongThuy@users.noreply.github.com>
2022-07-07 21:01:35 +0700

    user , css and js update

--- Motel-Management/web/assets/js/user-script.js
+++ Motel-Management/web/assets/js/user-script.js
@@ -141,22 +141,7 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
   };
 };
 
-// ===================Thuy detail=====================
-let vnd = Intl.NumberFormat("vi-VN", {
-  style: "currency",
-  currency: "VND",
-  useGrouping: true,
-});
-function price_format() {
-  $(".price-format").each(function () {
-    var $price = $(this).data("price"),
-      html = vnd.format($price);
-    $(this).html(html);
-  });
-}
-$(function () {
-  price_format();
-});
+
 $(function () {
   app_a.setUp();
 });
@@ -392,3 +377,19 @@ $(document).ready(function () {
                 window.location = $(this).data("href");
             });
         });
+        // ===================Thuy detail=====================
+let vnd = Intl.NumberFormat("vi-VN", {
+  style: "currency",
+  currency: "VND",
+  useGrouping: true,
+});
+function price_format() {
+  $(".price-format").each(function () {
+    var $price = $(this).data("price"),
+      html = vnd.format($price);
+    $(this).html(html);
+  });
+}
+$(function () {
+  price_format();
+});
